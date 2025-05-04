8be99c805342e60e325c88e22b476cbd3aff4c56
dorpsen <pdorp@outlook.com>
2025-05-04 15:55:24 +0200

    make sure the 'code' directory is in the search path of python

--- code/run_envelope.sh
+++ code/run_envelope.sh
@@ -1,2 +1,10 @@
-source LiveTradingBots/code/.venv/bin/activate
-python3 LiveTradingBots/code/strategies/envelope_kucoin/run.py
+# Make sure you are in the 'code' directory
+cd ~/LiveTradingBots/code
+source .venv/bin/activate
+
+# Run using the -m flag (module execution)
+python3 -m strategies.envelope_kucoin.run
+
+#old code
+#source LiveTradingBots/code/.venv/bin/activate
+#python3 LiveTradingBots/code/strategies/envelope_kucoin/run.py
